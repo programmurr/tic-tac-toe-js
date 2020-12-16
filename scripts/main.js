47eb504a9f965b4c6605d4c609439457525e188e
Conor <conorjm89@gmail.com>
2020-12-16 23:41:55 +0000

    Basic board appearance and functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,22 @@
-greeting = 'Success!';
-p = document.createElement('p');
-p.textContent = greeting;
-div = document.querySelector('div.hello');
-div.appendChild(p);
+// store the gameboard as an array inside of a Gameboard object (gameBoard module)
+// players will be stored in objects
+// object to control the flow of the game
+// displayController module
+// player factory
+
+const gameBoard = (function() {
+	let cells = [ '1', '2', '3', '4', '5', '6', '7', '8', '9' ];
+
+	// cache DOM
+	const boardCells = document.querySelectorAll('.cell');
+
+	_setupBoard();
+
+	function _setupBoard() {
+		for (let i = 0; i < boardCells.length; i++) {
+			boardCells[i].textContent = cells[i];
+		}
+	}
+
+	return { boardCells };
+})();
